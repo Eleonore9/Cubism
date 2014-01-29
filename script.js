23a0ad5834f94079ac59a1e18a3b493357709caf
Eleonore9 <eleonore.mayola@ymail.com>
2014-01-29 21:40:55 +0100

    starting my github page

---
+++ script.js
@@ -0,0 +1,69 @@
+/** Canvas playground for Codebar's tutorial Drawing in Canvas **/
+
+function draw() {
+ var canvas = document.getElementById('canvas-id');
+ var context = canvas.getContext("2d");
+
+ //draw items
+ //style
+ context.fillStyle = "red";
+ context.strokeStyle = "purple";
+ context.lineWidth = "1";
+ //Draw a rectangle (fill and stroke)
+ context.fillRect(100, 100, 50, 100);
+ context.strokeRect(100, 100, 50, 100);
+ //Draw a rect on upper rigt corner
+ context.strokeRect(300, 100, 50, 100);
+ //Draw a rect on the lower right corner
+ context.strokeRect(280, 350, 50, 100);
+ //Draw a line
+ context.strokeStyle = "blue";
+ context.moveTo(0,300);
+ context.lineTo(400,500);
+ context.stroke();
+ //Draw a rectangle by joining lines
+ context.beginPath();
+
+ context.lineWidth = 1;
+ context.strokeStyle = "purple";
+
+ context.moveTo(0,0);
+ context.lineTo(0,200);
+ context.lineTo(200,200);
+ context.lineTo(200,0);
+ context.lineTo(0,0);
+
+ context.stroke();
+ //Draw a circle 
+ context.beginPath();
+ context.arc(200, 100, 50, 0, Math.PI*2, true);
+ context.closePath();
+ context.strokeStyle = "green";
+ context.lineWidth = 4;
+ context.stroke();
+ //and another circle
+
+ context.beginPath();
+ context.arc(300, 250, 100, 0, Math.PI*2, true);
+ context.closePath();
+ context.fillStyle = "blue";
+ context.fill();
+ context.lineWidth = 2;
+ context.stroke();
+
+ context.save();
+ context.rotate(-2*Math.PI/180);
+
+ context.scale(1.5,1.5);
+ context.translate(40, 40);
+
+}
+
+function reset() {
+  var canvas = document.getElementById('canvas-id');
+  var context = canvas.getContext("2d");
+
+  // reset canvas
+  canvas.width = canvas.width;
+
+}
